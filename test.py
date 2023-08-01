642fbedbb54e3cd91c2b35403e2d54943a59ab0b
Maximiliano Russi Pinto <101850599+engsoftmax@users.noreply.github.com>
2023-07-31 22:53:24 -0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,3 @@
+#O curso da IBM Network Security & Database Vulnerabilities pediu para criar esse repositório específico com a impressão clássica do Hello World em python!
+#No futuro vou aproveitar e criar algo interessante! Let's Go!
+ print('Hello World!!!')
